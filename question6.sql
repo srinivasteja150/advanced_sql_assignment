a3ac86ccc9308204c7e7fe83c1b58fafa1475d2c
srinivasteja150 <122455311+srinivasteja150@users.noreply.github.com>
2023-02-27 16:29:18 +0530

    Update question6.sql

--- question6.sql
+++ question6.sql
@@ -1,4 +1,7 @@
 
-SELECT AVG(amount) as avg_sunday_revenue
+SELECT ROUND(AVG(sum_sunday_revenue),2) AS avg_sunday_revenue
+FROM(
+SELECT SUM(amount) AS sum_sunday_revenue
 FROM payment
-WHERE DAYOFWEEK(payment_date) = 1;
+WHERE DAYOFWEEK(payment_date) = 1
+GROUP BY DATE(payment_date)) a

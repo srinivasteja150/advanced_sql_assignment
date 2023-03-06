1e37ecb392e6d31f037815475e9854fc1292e7ce
srinivasteja150 <122455311+srinivasteja150@users.noreply.github.com>
2023-03-06 16:03:56 +0530

    Update question14.sql

--- question14.sql
+++ question14.sql
@@ -1,12 +1,12 @@
 
+
 SELECT p.payment_id,p.customer_id, p.staff_id, p.amount,
-    LAG(p.amount) OVER (PARTITION BY p.customer_id ORDER BY p.payment_id) AS prev_amount, 		-- gives prev_amount
-    LEAD(p.amount) OVER (PARTITION BY p.customer_id ORDER BY p.payment_id) AS next_amount,		-- gives next_amoount
-    (p.amount - (
-        SELECT AVG(amount) 
-        FROM payment 
-        WHERE staff_id = p.staff_id
-        AND customer_id = p.customer_id
-    )) AS ny_sales										-- calculates the net yield sales by that staff_id from the customer_id
+    LAG(p.amount) OVER (ORDER BY p.payment_id) AS prev_payment_amount, 		-- gives prev paymjent id amount
+    LEAD(p.amount) OVER ( ORDER BY p.payment_id) AS next_payment_amount,		-- gives next payment id amount
+    LAG(p.amount) OVER(PARTITION BY customer_id, staff_id order by payment_id) 
+			as py_sales, -- Prev payment done by same customer to staff
+    LEAD(p.amount) OVER(PARTITION BY customer_id, staff_id order by payment_id) 
+			as ny_sales  -- Next payment done by same customer to staff
 FROM payment p
 WHERE p.customer_id = '269' AND p.staff_id = '1';
+
